c1eef5d85489d8d648b39dc6827620177369581c
Gerard Ganza <gerard,ganza@makeitissi.com>
2024-02-13 17:50:05 +0800

    Removed header from sidebar menu

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -1,7 +1,6 @@
 <nav class="sb-sidenav accordion marsman-bg-color-dark" id="sidenavAccordion">
     <div class="sb-sidenav-menu">
-        <div class="nav">
-            <div class="sb-sidenav-menu-heading txt-secondary">Core</div>
+        <div class="nav mt-3">
             <a class="nav-link text-white" href="{{ route('home') }}">
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Dashboard
